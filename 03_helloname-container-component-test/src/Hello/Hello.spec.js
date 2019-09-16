8681d5db019658e4ead5ea5e0c68fbbf76ca31dd
Milan Burgmann <milan.burgmann@dw.com>
2019-09-16 11:47:19 +0200

    added test for hello component

--- 03_helloname-container-component-test/src/Hello/Hello.spec.js
+++ 03_helloname-container-component-test/src/Hello/Hello.spec.js
@@ -1,9 +1,15 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import { shallow } from "enzyme";
 import Hello from "./Hello";
 
 describe("hello component", () => {
   it("should render without crash", () => {
-    // TODO add test here
+    const div = document.createElement('div');
+    ReactDOM.render(<Hello name='Tester' age={2} index={1} changeName={console.log} />, div)
   });
+
+  it("should render shallow", () => {
+    shallow(<Hello name='Tester' age={2} index={1} changeName={console.log} />);
+  })
 });
